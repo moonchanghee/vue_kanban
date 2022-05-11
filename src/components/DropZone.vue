35f530d13720a35280c80f0585bce99dd47017b9
changhee <changhee@urbanbase.com>
2022-05-11 17:55:40 +0900

    dropzone template 분리, 메서드 수정

---
+++ src/components/DropZone.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dropzone"
+       @drop = "onDrop($event)"
+       @dragover = "onDragover($event)"
+       @dragleave  = "onDragleave($event)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    todoState : String
+  },
+  methods : {
+    onDrop(e){
+      let data = {
+        event : e ,
+        todoState : this.todoState
+      }
+      this.$store.commit('onDrop' , data )
+      e.target.classList.remove("dropzone_active")
+    },
+    onDragover(e){
+      e.preventDefault()
+      e.target.classList.add("dropzone_active")
+    },
+    onDragleave(e){
+      e.target.classList.remove("dropzone_active")
+    }
+  }
+}
+
+</script>
+
+<style>
+.dropzone{
+  height: 7px;
+  transition: background 0.15s, height 0.15s;
+}
+
+.dropzone_active {
+  height: 30px;
+  background: rgba(0, 0, 0, 0.25);
+}
+
+</style>
