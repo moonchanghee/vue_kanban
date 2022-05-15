33a6c4c33fc916f613f916341131077aaeba8d2a
moonchanghee <ckdgml228@gmail.com>
2022-05-16 00:04:45 +0900

    update, additem 메서드 삭제 , onClickButton 메서드 추가

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,31 +2,24 @@
   <div class="modal hidden"  v-if = "this.$store.state.modalState">
     <div>
       <label for="title" >제목:</label>
-      <input v-if="this.$store.state.updateTodo === ''" v-bind:value="todoTitle" v-on:input="updateTitle">
-      <input v-else v-bind:value="todoTitle = this.$store.state.updateTodo.todoTitle" v-on:input="updateTitle">
+      <input v-model = "todoTitle">
     </div>
     <div>
       <label for="date" >완료일: </label>
-      <input v-if="this.$store.state.updateTodo === ''" v-bind:value="todoDate" v-on:input="updateDate">
-      <input v-else v-bind:value="todoDate = this.$store.state.updateTodo.todoDate" v-on:input="updateDate">
+      <input v-model = "todoDate">
     </div>
     <div>
-      우선순위<select v-model="prioritySelected" v-on:input="updatePriority">
-      <option v-for="option in stateOptions" v-bind:value="option.value" >
-        {{ option.text }}
-      </option>
+      우선순위<select  v-model="prioritySelected" >
+        <option :value="list" v-for="list in priorityOptions">{{ list.text }}</option>
     </select>
-      상태<select v-model="stateSelected" v-on:input="updatedState">
-      <option v-for="option in priorityOptions" v-bind:value="option.value">
-        {{ option.text }}
-      </option>;
+      상태 <select  v-model="stateSelected" >
+      <option :value="list" v-for="list in stateOptions">{{ list.text }}</option>
     </select>
       <br/>
       <br/>
-      <textarea cols="45" rows="10" v-if="this.$store.state.updateTodo === ''" v-bind:value="todoContents = ''" v-on:input="updateContents"></textarea>
-      <textarea cols="45" rows="10" v-else v-bind:value="todoContents = this.$store.state.updateTodo.todoContents" v-on:input="updateContents"></textarea>
-      <div v-if="this.$store.state.updateTodo === ''" class="closeBtn"  @click="onClickAddItem">작성완료</div>
-      <div v-else @click="onClickUpdateItem">수정완료</div>
+      <textarea cols="45" rows="10" v-model = "todoContents"></textarea>
+      <div v-if="this.$store.state.updateTodo === ''" class="closeBtn"  @click="onClickButton('addItem',uuid())">작성완료</div>
+      <div v-else @click="onClickButton('updateItem',this.$store.state.updateTodo.id)">수정완료</div>
     </div>
   </div>
 </template>
@@ -38,75 +31,72 @@ export default {
     return{
       todoTitle: "",
       todoDate : "",
-      todoState : "",
-      todoPriority : "",
       todoContents : "",
-      todoPriorityNum : "",
-      stateSelected: '0',
-      prioritySelected: '0',
-      stateOptions: [
+      stateSelected: '',
+      prioritySelected:'',
+      priorityOptions: [
         { text: '선택', value: 0 },
         { text: '높음', value: 1 },
         { text: '중간', value: 2 },
         { text: '낮음', value: 3 }
       ],
-      priorityOptions: [
+      stateOptions: [
         { text: '선택', value: 0 },
         { text: 'ToDo', value: 'ToDo' },
         { text: 'In_progress', value: 'In_progress' },
         { text: 'Done', value: 'Done' }
-      ]
+      ],
     }
   },
   methods: {
-    updateTitle: function(e) {
-      this.todoTitle = e.target.value
-    },
-    updateDate: function(e) {
-      this.todoDate = e.target.value
-    },
-    updatedState: function(e) {
-      this.todoState = e.target.value
-    },
-    updatePriority: function(e) {
-      this.todoPriorityNum = e.target.value
-      this.todoPriority = e.target[e.target.value].text
-    },
-    updateContents : function(e){
-      this.todoContents = e.target.value
-    },
-    getItemList(e){
-      return {
-        id : e,
-        todoTitle : this.todoTitle,
-        todoDate : this.todoDate,
-        todoPriority: this.todoPriority,
-        todoContents: this.todoContents,
-        todoPriorityNum: this.todoPriorityNum,
-        todoState : this.stateSelected,
-        prioritySelected:this.prioritySelected
+    checkData(){
+      let reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/;
+      if (reg.test(this.todoTitle)) {
+        return "제목에는 특수문자를 사용할 수 없습니다"
+      }else if (reg.test(this.todoContents)) {
+        return "내용에는 특수문자를 사용할 수 없습니다"
+      } else if (this.todoTitle.length > 30) {
+        return "제목은 30자를 초과할 수 없습니다"
+      }else if (this.todoContents.length > 150) {
+        return "내용은 150자를 초과할 수 없습니다"
+      }else if (this.prioritySelected === "") {
+        return "우선순위를 선택해주세요"
+      } else if (this.stateSelected === "") {
+        return "상태를 선택해주세요"
+      } else{
+        return 0
       }
-    }
-    ,
+    },
     uuid : function(){
       return 'xx'.replace(/[xy]/g, function(c) {
         let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
       });
     },
-    onClickUpdateItem(){
-      let id = this.$store.state.updateTodo.id
-      this.$store.dispatch('updateItem', this.getItemList(id))
-    },
-
-    onClickAddItem(){
-      let id = this.uuid()
-      this.$store.dispatch('addItem', this.getItemList(id))
+    onClickButton(e,id){
+      let payload = {
+        id: id,
+        todoTitle: this.todoTitle,
+        todoDate: this.todoDate,
+        todoPriority: this.prioritySelected.text,
+        todoContents: this.todoContents,
+        todoState: this.stateSelected.value,
+        todoPriorityNum: this.prioritySelected.value,
+      }
+      if(this.checkData()){
+        alert(this.checkData())
+      }
+      else{
+        this.$store.dispatch(e, payload)
+        this.todoTitle= ""
+        this.todoDate = ""
+        this.todoContents = ""
+        this.stateSelected= ""
+        this.prioritySelected= ""
+      }
     }
   }
-
 };
-
 </script>
 
 <style>
@@ -122,3 +112,4 @@ export default {
 }
 
 </style>
+
